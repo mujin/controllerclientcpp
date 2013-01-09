1204cb142f6c36e75fd9b1dcc9f6661fd0213efa
rdiankov <rosen.diankov@gmail.com>
2013-01-09 10:22:38 +0900

    fixed visual studio 2005 compilation, updated to v0.1.1

--- src/mujincontrollerclient.cpp
+++ src/mujincontrollerclient.cpp
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#define WIN32_LEAN_AND_MEAN
 #include <mujincontrollerclient/mujincontrollerclient.h>
 
 #include <boost/thread/mutex.hpp>
