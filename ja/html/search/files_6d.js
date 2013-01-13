842ef9d4a9cb973d42c4d1c3ccc6b3b59e0bc3df
rdiankov <rosen.diankov@gmail.com>
2013-01-13 14:50:24 +0900

    updated docs

--- ja/html/search/files_6d.js
+++ ja/html/search/files_6d.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['mujincontrollerclient_2eh',['mujincontrollerclient.h',['../mujincontrollerclient_8h.html',1,'']]]
+  ['mujinclienttest_2ecpp',['mujinclienttest.cpp',['../mujinclienttest_8cpp.html',1,'']]],
+  ['mujincontrollerclient_2eh',['mujincontrollerclient.h',['../mujincontrollerclient_8h.html',1,'']]],
+  ['mujinimportscene_2ecpp',['mujinimportscene.cpp',['../mujinimportscene_8cpp.html',1,'']]]
 ];
--- ja/html/search/functions_6d.js
+++ ja/html/search/functions_6d.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['main',['main',['../mujinclienttest_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'main(int argc, char **argv):&#160;mujinclienttest.cpp'],['../mujinimportscene_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'main(int argc, char **argv):&#160;mujinimportscene.cpp']]],
   ['message',['message',['../classmujinclient_1_1mujin__exception.html#a42a267693efdb3a913fb4f01d4294ecb',1,'mujinclient::mujin_exception']]],
   ['mujin_5fexception',['mujin_exception',['../classmujinclient_1_1mujin__exception.html#a7ea7613215522a2c146e1bc3f988806c',1,'mujinclient::mujin_exception::mujin_exception()'],['../classmujinclient_1_1mujin__exception.html#a2daa7944c8c0e1af0ed1c8c6a38823c2',1,'mujinclient::mujin_exception::mujin_exception(const std::string &amp;s, MujinErrorCode error=MEC_Failed)']]]
 ];
