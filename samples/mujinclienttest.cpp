c88892a049b31d7ec2fe5376b8d8f4d39c0bb78f
rdiankov <rosen.diankov@gmail.com>
2013-01-13 14:51:37 +0900

    doc fixes

--- samples/mujinclienttest.cpp
+++ samples/mujinclienttest.cpp
@@ -1,16 +1,8 @@
 // -*- coding: utf-8 -*-
-// Copyright (C) 2012-2013 MUJIN Inc. <rosen.diankov@mujin.co.jp>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/** \example mujinclienttest.cpp
+
+    Shows how to get the result data data from an already generated task and optimization results.
+ */
 #include <mujincontrollerclient/mujincontrollerclient.h>
 
 #include <iostream>
@@ -22,10 +14,16 @@ using namespace mujinclient;
 int main(int argc, char ** argv)
 {
     if( argc < 2 ) {
-        printf("need username:password. Example: mujinclienttest myuser:mypass\n");
+        printf("need username:password. Example: mujinclienttest myuser:mypass [url]\n\nurl - [optional] For example https://controller.mujin.co.jp/\n");
         return 1;
     }
-    ControllerClientPtr controller = CreateControllerClient(argv[1]);
+    ControllerClientPtr controller;
+    if( argc > 2 ) {
+        controller = CreateControllerClient(argv[1], argv[2]);
+    }
+    else {
+        controller = CreateControllerClient(argv[1]);
+    }
 
     // get all supported keys
     std::vector<std::string> scenekeys;
@@ -54,14 +52,10 @@ int main(int argc, char ** argv)
         return 0;
     }
     TaskResourcePtr task(new TaskResource(controller, taskkeys.at(0)));
-    //task = scene->GetOrCreateTaskFromName("task0");
 
     cout << "got task " << task->Get("name") << endl;
     cout << "program is " << task->Get("taskgoalxml") << endl;
-    // execute task
-    //task->Execute();
-    // check if task is complete
-    //task->GetRunTimeStatus()
+
     PlanningResultResourcePtr result = task->GetResult();
     std::map<std::string, Transform> transforms;
     if( !!result ) {
@@ -71,7 +65,7 @@ int main(int argc, char ** argv)
     // get the first optimization
     std::vector<std::string> optimizationkeys;
     task->GetOptimizationPrimaryKeys(optimizationkeys);
-    //OptimizationResourcePtr optimization = task->GetOrCreateOptimizationFromName(optimizationkeys.at(0));
+
     OptimizationResourcePtr optimization(new OptimizationResource(controller, optimizationkeys.at(0)));
     cout << "found optimization " << optimization->Get("name") << endl;
 
--- samples/mujinimportscene.cpp
+++ samples/mujinimportscene.cpp
@@ -1,16 +1,8 @@
 // -*- coding: utf-8 -*-
-// Copyright (C) 2012-2013 MUJIN Inc. <rosen.diankov@mujin.co.jp>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/** \example mujinimportscene.cpp
+
+    Shows how to import a scene and query a list of the instance objects inside the scene. Note that querying only works for MUJIN COLLADA scenes.
+ */
 #include <mujincontrollerclient/mujincontrollerclient.h>
 
 #include <iostream>
@@ -20,7 +12,7 @@ using namespace mujinclient;
 int main(int argc, char ** argv)
 {
     if( argc < 2 ) {
-        printf("need username:password. Example: mujinclienttest myuser:mypass [url]\n\nurl - [optional] For example https://controller.mujin.co.jp/\n");
+        printf("need username:password. Example: mujinimportscene myuser:mypass [url]\n\nurl - [optional] For example https://controller.mujin.co.jp/\n");
         return 1;
     }
     ControllerClientPtr controller;
