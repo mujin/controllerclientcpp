88c3f284ebb4839f85e494033f124fd4e4dc9343
yfukaya <yfukaya@fa.denso.co.jp>
2013-01-21 16:21:44 +0900

    コマンドラインに　myproxy:myport　を指定可能にしました

--- samples/mujinexecutetask.cpp
+++ samples/mujinexecutetask.cpp
@@ -14,11 +14,14 @@ using namespace mujinclient;
 int main(int argc, char ** argv)
 {
     if( argc < 2 ) {
-        std::cout << "need username:password. Example: mujinclienttest myuser:mypass [url]\n\nurl - [optional] For example https://controller.mujin.co.jp/" << std::endl;
+		std::cout << "need username:password. Example: mujinclienttest myuser:mypass [url [,myproxy:myport]]\n\nurl - [optional] For example https://controller.mujin.co.jp/\nmyproxy:myport - [optional] For example 10.6.227.36:8080" << std::endl;
         return 1;
     }
     try {
         ControllerClientPtr controller;
+        if( argc > 3 ) {
+            controller = CreateControllerClient(argv[1], argv[2], argv[3]);
+        } else
         if( argc > 2 ) {
             controller = CreateControllerClient(argv[1], argv[2]);
         }
