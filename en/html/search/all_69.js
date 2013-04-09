908ff83b2cc9093a5e07438ea2cf845f5bea6325
rdiankov <rosen.diankov@gmail.com>
2013-04-10 01:27:58 +0900

    updated documentation

--- en/html/search/all_69.js
+++ en/html/search/all_69.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['ignorebasecollision',['ignorebasecollision',['../structmujinclient_1_1RobotPlacementOptimizationParameters.html#a575aa32b674de94b9262483b6aab5d16',1,'mujinclient::RobotPlacementOptimizationParameters']]],
   ['ignorefigure',['ignorefigure',['../classmujinclient_1_1ITLPlanningTaskParameters.html#abb44c20d97f306aa72bac068f373aa1b',1,'mujinclient::ITLPlanningTaskParameters']]],
-  ['importscenetocollada',['ImportSceneToCOLLADA',['../classmujinclient_1_1ControllerClient.html#a786a3a92770adfd3e4d5e559ba795d67',1,'mujinclient::ControllerClient']]],
+  ['importscenetocollada_5futf16',['ImportSceneToCOLLADA_UTF16',['../classmujinclient_1_1ControllerClient.html#a295e6e70e2f4e23323bc96d3b25b21d1',1,'mujinclient::ControllerClient']]],
+  ['importscenetocollada_5futf8',['ImportSceneToCOLLADA_UTF8',['../classmujinclient_1_1ControllerClient.html#a2769dbecbcf45e1bc6733209968b2a9f',1,'mujinclient::ControllerClient']]],
   ['instanceobjectstate',['InstanceObjectState',['../structmujinclient_1_1InstanceObjectState.html',1,'mujinclient']]],
   ['instobject',['InstObject',['../classmujinclient_1_1SceneResource_1_1InstObject.html#ad1fe602d3cdb40c1623885f9d12f69d4',1,'mujinclient::SceneResource::InstObject']]],
   ['instobject',['InstObject',['../classmujinclient_1_1SceneResource_1_1InstObject.html',1,'mujinclient::SceneResource']]],
--- ja/html/search/functions_69.js
+++ ja/html/search/functions_69.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['importscenetocollada',['ImportSceneToCOLLADA',['../classmujinclient_1_1ControllerClient.html#a786a3a92770adfd3e4d5e559ba795d67',1,'mujinclient::ControllerClient']]],
+  ['importscenetocollada_5futf16',['ImportSceneToCOLLADA_UTF16',['../classmujinclient_1_1ControllerClient.html#a295e6e70e2f4e23323bc96d3b25b21d1',1,'mujinclient::ControllerClient']]],
+  ['importscenetocollada_5futf8',['ImportSceneToCOLLADA_UTF8',['../classmujinclient_1_1ControllerClient.html#a2769dbecbcf45e1bc6733209968b2a9f',1,'mujinclient::ControllerClient']]],
   ['instobject',['InstObject',['../classmujinclient_1_1SceneResource_1_1InstObject.html#ad1fe602d3cdb40c1623885f9d12f69d4',1,'mujinclient::SceneResource::InstObject']]],
   ['itlplanningtaskparameters',['ITLPlanningTaskParameters',['../classmujinclient_1_1ITLPlanningTaskParameters.html#a19ad2c4ba470be7213c4cc91546f0285',1,'mujinclient::ITLPlanningTaskParameters']]]
 ];
