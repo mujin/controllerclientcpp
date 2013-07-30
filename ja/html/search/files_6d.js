f52589a2571d79da36bfc3142c52506a6fc522e5
rdiankov <rosen.diankov@gmail.com>
2013-07-30 11:41:33 +0900

    updated documentation

--- ja/html/search/files_6d.js
+++ ja/html/search/files_6d.js
@@ -5,6 +5,7 @@ var searchData=
   ['mujincontrollerclient_2eh',['mujincontrollerclient.h',['../mujincontrollerclient_8h.html',1,'']]],
   ['mujinexecutetask_2ecpp',['mujinexecutetask.cpp',['../mujinexecutetask_8cpp.html',1,'']]],
   ['mujinexecutetask_5ffast_2ecpp',['mujinexecutetask_fast.cpp',['../mujinexecutetask__fast_8cpp.html',1,'']]],
+  ['mujinexecutetask_5frobodia_2ecpp',['mujinexecutetask_robodia.cpp',['../mujinexecutetask__robodia_8cpp.html',1,'']]],
   ['mujingetresults_2ecpp',['mujingetresults.cpp',['../mujingetresults_8cpp.html',1,'']]],
   ['mujinideal_5fdensowave_2ecpp',['mujinideal_densowave.cpp',['../mujinideal__densowave_8cpp.html',1,'']]],
   ['mujinimportscene_2ecpp',['mujinimportscene.cpp',['../mujinimportscene_8cpp.html',1,'']]],
