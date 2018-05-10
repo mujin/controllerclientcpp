64b94ba8b2a3e5626b366ee5014165547edf0ffb
Taiju Yamada <taiju.yamada@mujin.co.jp>
2018-05-10 17:42:15 +0900

    Fixed SetTaskParameters

--- src/controllerclientimpl.cpp
+++ src/controllerclientimpl.cpp
@@ -887,7 +887,7 @@ int ControllerClientImpl::CallPutSTL(const std::string& relativeuri, const std::
 
 int ControllerClientImpl::CallPutJSON(const std::string& relativeuri, const std::string& data, boost::property_tree::ptree& pt, int expectedhttpcode, double timeout)
 {
-    return _CallPut(relativeuri, static_cast<const void*>(&data), data.size(), pt, _httpheadersjson, expectedhttpcode, timeout);
+    return _CallPut(relativeuri, static_cast<const void*>(&data[0]), data.size(), pt, _httpheadersjson, expectedhttpcode, timeout);
 }
 
 void ControllerClientImpl::CallDelete(const std::string& relativeuri, double timeout)
