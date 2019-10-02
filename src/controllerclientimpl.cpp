8a5bf09b187458bcb9ed7f7aa3886ebb3b90463b
Taiju Yamada <taiju.yamada@mujin.co.jp>
2019-10-02 12:03:14 +0900

    avoid SIGSEGV when DELETE fails, where error message was written in uninitialized FILE*

--- src/controllerclientimpl.cpp
+++ src/controllerclientimpl.cpp
@@ -767,11 +767,19 @@ void ControllerClientImpl::CallDelete(const std::string& relativeuri, int expect
     CURL_OPTION_SAVE_SETTER(_curl, CURLOPT_HTTPHEADER, NULL, _httpheadersjson);
     CURL_OPTION_SAVE_SETTER(_curl, CURLOPT_URL, NULL, _uri.c_str());
     CURL_OPTION_SAVE_SETTER(_curl, CURLOPT_CUSTOMREQUEST, NULL, "DELETE");
+    _buffer.clear();
+    _buffer.str("");
+    CURL_OPTION_SAVE_SETTER(_curl, CURLOPT_WRITEFUNCTION, NULL, _WriteStringStreamCallback);
+    CURL_OPTION_SAVE_SETTER(_curl, CURLOPT_WRITEDATA, NULL, &_buffer);
     CURL_PERFORM(_curl);
     long http_code = 0;
     CURL_INFO_GETTER(_curl, CURLINFO_RESPONSE_CODE, &http_code);
     if( http_code != expectedhttpcode ) {
-        throw MUJIN_EXCEPTION_FORMAT("HTTP DELETE to '%s' returned HTTP status %s", relativeuri%http_code, MEC_HTTPServer);
+        rapidjson::Document d;
+        ParseJson(d, _buffer.str());
+        std::string error_message = GetJsonValueByKey<std::string>(d, "error_message");
+        std::string traceback = GetJsonValueByKey<std::string>(d, "traceback");
+        throw MUJIN_EXCEPTION_FORMAT("HTTP DELETE to '%s' returned HTTP status %s: %s", relativeuri%http_code%error_message, MEC_HTTPServer);
     }
 }
 
