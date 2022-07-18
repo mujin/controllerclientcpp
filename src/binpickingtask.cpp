580281d68fca66c7054915517a3ebb965f5e31c5
rosen <rosen.diankov@mujin.co.jp>
2022-07-19 01:26:32 +0900

    use transferSpeedPostMult

--- src/binpickingtask.cpp
+++ src/binpickingtask.cpp
@@ -571,7 +571,7 @@ void BinPickingTaskResource::ResultGetBinpickingState::Parse(const rapidjson::Va
     registerMinViableRegionInfo.objectWeight = GetJsonValueByPath<double>(v, "/registerMinViableRegionInfo/objectWeight", 0);
     registerMinViableRegionInfo.sensortimestamp = GetJsonValueByPath<uint64_t>(v, "/registerMinViableRegionInfo/sensortimestamp", 0);
     registerMinViableRegionInfo.robotDepartStopTimestamp = GetJsonValueByPath<double>(v, "/registerMinViableRegionInfo/robotDepartStopTimestamp", 0);
-    registerMinViableRegionInfo.transferSpeedMult = GetJsonValueByPath<double>(v, "/registerMinViableRegionInfo/transferSpeedMult", 1.0);
+    registerMinViableRegionInfo.transferSpeedPostMult = GetJsonValueByPath<double>(v, "/registerMinViableRegionInfo/transferSpeedPostMult", 1.0);
     {
         registerMinViableRegionInfo.graspModelInfo.SetNull();
         registerMinViableRegionInfo.graspModelInfo.GetAllocator().Clear();
